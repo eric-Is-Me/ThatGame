1c4c98a2516dd79d2e91464eb29b8c6d60a8d82f
Eric <eric.chaing.official@gmail.com>
2017-06-13 20:28:11 -0700

    First Commit
    Touchscreen Controls Script named PlayerController.cs and input detector script named DetectInputDirection.cs

---
+++ That Game/That Game/Assets/Scripts/InputDetector.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum InputDirection {
+    Left, Right, Up, Down
+}
+
+public interface InputDetector{
+    InputDirection? DetectInputDirection();
+}
