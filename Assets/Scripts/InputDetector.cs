cdd5b1382b23c3cb9c118c777d0294b52934c552
Eric <eric.chaing.official@gmail.com>
2017-11-22 17:39:39 -0800

    Added jump features and completed infinite spawner

---
+++ Assets/Scripts/InputDetector.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum InputDirection {
+    Left, Right, Up, Down, None
+}
+
+public interface InputDetector{
+    InputDirection? DetectInputDirection();
+}
