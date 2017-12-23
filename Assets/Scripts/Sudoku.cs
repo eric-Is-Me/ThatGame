ffcc5bc0923d09df1e3f019f8c3b06dc261fe070
Eric <eric.chaing.official@gmail.com>
2017-12-22 18:05:25 -0800

    Road now recursively commits sudoku

---
+++ Assets/Scripts/Sudoku.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sudoku : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+        Destroy(gameObject, 2.5f);
+	}
+}
