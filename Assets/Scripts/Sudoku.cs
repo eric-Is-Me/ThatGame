a040f62f8e10a1b18eca38fbfb40e3c18888b139
Eric <eric.chaing.official@gmail.com>
2018-01-18 12:39:18 -0800

    Revised object spawning script
    Included to spawn obstacles randomly

--- Assets/Scripts/Sudoku.cs
+++ Assets/Scripts/Sudoku.cs
@@ -1,31 +1,10 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Sudoku : MonoBehaviour {
 	// Update is called once per frame
-<<<<<<< HEAD
 	void Update () {
-=======
-	void Update () {
->>>>>>> a8210bcc1823e2edcb641e0a48c17cd1755ab8f8
         Destroy(gameObject, 2.5f);
 	}
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Sudoku : MonoBehaviour {
-	// Update is called once per frame
-<<<<<<< HEAD
-	void Update () {
-=======
-	void Update () {
->>>>>>> a8210bcc1823e2edcb641e0a48c17cd1755ab8f8
-        Destroy(gameObject, 2.5f);
-	}
-}
->>>>>>> 0.2-quick-fix
