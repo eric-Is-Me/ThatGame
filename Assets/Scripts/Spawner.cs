23c5234039ccb7ba6fd4c1997f4d4cdbf9cdb76d
Eric Chaing <chaingisme@gmail.com>
2018-01-17 17:46:18 -0800

    Revised Spawner

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    public Transform Path;
+    public Transform Pillar;
+
+	//Creates new objects in front of player model on trigger
+	void OnTriggerEnter(Collider Player){
+		Vector3 pos = new Vector3(1, 0, 9);
+		var playerZ = GameObject.Find ("Player").transform.position.z;
+		Vector3 finalPos = pos + new Vector3 (0, 0, playerZ);
+		Instantiate (Path, finalPos, Quaternion.identity);
+
+        Vector3 randPos = new Vector3(Random.Range(-3.0f, 1.0f), 1, Random.Range(playerZ, playerZ + 9.0f));
+        Instantiate(Pillar, randPos, Quaternion.identity);
+    }
+}
---
+++ That Game/That Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+
+public enum State
+{
+    SwipeStarted, SwipeNotStarted
+}
+public class PlayerController : MonoBehaviour, InputDetector {
+
+    private State state = State.SwipeNotStarted;
+    private Vector2  startPoint;
+    private DateTime timeSwipeStarted;
+    private TimeSpan maxSwipeTime = TimeSpan.FromSeconds(1);
+    private TimeSpan minSwipeTime = TimeSpan.FromMilliseconds(100);
+
+    public InputDirection? DetectInputDirection()
+    {
+        if(state == State.SwipeNotStarted)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                timeSwipeStarted = DateTime.Now;
+                state = State.SwipeStarted;
+                startPoint = Input.mousePosition;
+            }
+        }
+        else if(state == State.SwipeStarted)
+        {
+            if(Input.GetMouseButtonUp(0))
+            {
+                TimeSpan timeDifference = DateTime.Now - timeSwipeStarted;
+                if(timeDifference <= maxSwipeTime && timeDifference >= minSwipeTime)
+                {
+                    Vector2 mousePosition = Input.mousePosition;
+                    Vector2 differenceVector = mousePosition - startPoint;
+                    float angle = Vector2.Angle(differenceVector, Vector2.right);
+                    Vector3 cross = Vector3.Cross(differenceVector, Vector2.right);
+
+                    if (cross.z > 0)
+                        angle = 360 - angle;
+                    state = State.SwipeNotStarted;
+
+                    if ((angle >= 315 && angle <= 360 || angle >= 0 && angle <= 45))
+                        return InputDirection.Right;
+                    if ((angle >= 45 && angle <= 135))
+                        return InputDirection.Up;
+                    if ((angle >= 135 && angle <= 225))
+                        return InputDirection.Left;
+                    else
+                        return InputDirection.Down;
+                }
+            }
+        }
+        return null;
+    }
+}
